4b9bb41d4140da80f7246b4e2ab0b748818d4067
splitice <mat999@gmail.com>
2014-11-19 21:39:23 +1100

    Added return code validation and delayed exception throwing to not interrupt legacy flow.

--- src/Splitice/ErrorToException.php
+++ src/Splitice/ErrorToException.php
@@ -1,22 +1,44 @@
 <?php
 namespace Splitice;
 
+/**
+ * Class ErrorToException for the translation of legacy errors to Exceptions
+ * @package Splitice
+ */
 class ErrorToException {
     /**
-     * E_ERROR | E_USER_ERROR | E_USER_WARNING | E_COMPILE_ERROR | E_CORE_ERROR | E_RECOVERABLE_ERROR
+     * E_USER_WARNING | E_USER_ERROR | E_ERROR | E_COMPILE_ERROR | E_CORE_ERROR | E_RECOVERABLE_ERROR
      */
     const DEFAULT_ERRORS = 4945;
 
-    public static function handle($functor, $errors = -1, $exception = '\\ErrorException'){
+    /**
+     * Call $functor, if the output matches $validation_function or if $validation_function is NULL
+     * then throw a $exception if an error matching $errors is found
+     *
+     * @param callable|$functor
+     * @param callable|null $validation_function
+     * @param int $errors
+     * @param string $exception
+     */
+    private static function handle($functor, $validation_function = null, $errors = -1, $exception = '\\ErrorException'){
         if($errors == -1){
             $errors = self::DEFAULT_ERRORS;
         }
 
-        $previous_handler = NULL;
-        $error_handler = function ($errno, $errstr, $errfile, $errline ) use($exception, $errors, &$previous_handler) {
+        $ex = $previous_handler = NULL;
+        $error_handler = function ($errno, $errstr, $errfile, $errline )
+                            use($exception, $errors, &$previous_handler, &$ex, $validation_function) {
             if($errno & $errors) {
-                throw new $exception($errstr, 0, $errno, $errfile, $errline);
+                $ex = new $exception($errstr, 0, $errno, $errfile, $errline);
+
+                //Is error, we need to do something now
+                if($errno & (E_USER_ERROR | E_ERROR | E_COMPILE_ERROR | E_CORE_ERROR | E_RECOVERABLE_ERROR)){
+                    if($validation_function == NULL || $validation_function($result)){
+                        throw $ex;
+                    }
+                }
             }
+
 
             if($previous_handler == NULL){
                 return false;
@@ -26,7 +48,13 @@ class ErrorToException {
 
         $previous_handler = set_error_handler($error_handler);
 
-        $functor();
+        $result = $functor();
+
+        if($ex != null){
+            if($validation_function == NULL || $validation_function($result)){
+                throw $ex;
+            }
+        }
 
         restore_error_handler();
     }
